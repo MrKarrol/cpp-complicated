17765f95e930ac3d7fdbfbd392dc088c169d097d
KrisKaroll <g.mishell@gmail.com>
2021-10-03 18:12:02 +0700

    ha3: task 2 complited

--- ha3/ha3.cpp
+++ ha3/ha3.cpp
@@ -2,6 +2,8 @@
 #include <list>
 #include <random>
 
+#include "matrix.h"
+
 
 void AddRealAverage(std::list<double>& list)
 {
@@ -11,10 +13,12 @@ void AddRealAverage(std::list<double>& list)
 	list.push_back(average / list.size());
 }
 
+
 int main()
 {
 	// task 1
 	{
+		std::cout << "******* TASK 1 *******" << std::endl;
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_real_distribution<double> urd(0, 100);
@@ -35,6 +39,34 @@ int main()
 		for (const auto& item : list)
 			std::cout << item << " ";
 		std::cout << std::endl;
+	}
+	// task 2
+	{
+		std::cout << "******* TASK 2 *******" << std::endl;
+
+		try
+		{
+			{
+				Matrix matrix(2, { 3, -5, 2, 6 });
+				std::cout << "Determinant of " << matrix << "is " << matrix.GetDeterminant() << std::endl;
+			}
+			{
+				Matrix matrix(3, { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+				std::cout << "Determinant of " << matrix << "is " << matrix.GetDeterminant() << std::endl;
+			}
+			{
+				Matrix matrix(4, { 3, 5, -2, -25, 32, 15, 81, 3, 2, 23, 3, 4, 92, -9, -3, -1 });
+				std::cout << "Determinant of " << matrix << "is " << matrix.GetDeterminant() << std::endl;
+			}
+			{
+				Matrix matrix(5, { -1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, -15, 16, -17, 18, -19, 20, -21, 22, -23, 24, -25 });
+				std::cout << "Determinant of " << matrix << "is " << matrix.GetDeterminant() << std::endl;
+			}
+		}
+		catch (std::exception& e)
+		{
+			std::cerr << "ERROR!! " << e.what() << std::endl;
+		}
 	}
 	return 0;
 }
--- ha3/matrix.h
+++ ha3/matrix.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <vector>
+#include <cstdint>
+#include <iostream>
+
+
 class Matrix
 {
 public:
-	Matrix(int x, int y);
+	Matrix(uint8_t side, const std::vector<int> &data);
+	Matrix(uint8_t side, std::vector<int>&& data);
+
+	int GetDeterminant() const;
+	Matrix GetMinor(uint8_t x, uint8_t y) const;
+	int Get(uint8_t x, uint8_t y) const;
+	uint8_t GetSide() const noexcept;
+
+	friend std::ostream& operator << (std::ostream& out, const Matrix& matrix);
+
+private:
+	const uint8_t m_side;
+	const std::vector<int> m_matrix;
 
 };
+
+std::ostream& operator << (std::ostream& out, const Matrix& matrix);
