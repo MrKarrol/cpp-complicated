a18e7c05684bdb462cbe4f456835d3a70141d7cb
KrisKaroll <g.mishell@gmail.com>
2021-10-03 21:16:54 +0700

    ha4: all complited

--- ha4/ha4.cpp
+++ ha4/ha4.cpp
@@ -2,6 +2,8 @@
 #include <random>
 #include <list>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 
 #include "insert.h"
 
@@ -39,6 +41,33 @@ int main()
 			insert_sorted(vec, elem_to_insert);
 			std::cout << "sorted list after insertion" << std::endl << vec << std::endl;
 		}
+	}
+	// task 2
+	{
+		std::cout << "***** TASK 2 *****" << std::endl;
+
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<double> urd(-100.f, 100.f);
+
+		std::vector<double> analog(100);
+		std::generate(std::begin(analog), std::end(analog), [&urd, &gen]() { return urd(gen); });
+		std::vector<int> digital(analog.size());
+		std::copy(std::begin(analog), std::end(analog), std::begin(digital));
+
+		std::cout << "analog" << std::endl;
+		std::copy(std::begin(analog), std::end(analog), std::ostream_iterator<double>{std::cout, " "});
+		std::cout << std::endl;
+		std::cout << "digital" << std::endl;
+		std::copy(std::begin(digital), std::end(digital), std::ostream_iterator<int>{std::cout, " "});
+		std::cout << std::endl;
+
+		std::vector<double> errors(analog.size());
+		std::transform(std::cbegin(analog), std::cend(analog), std::cbegin(digital), std::begin(errors), std::minus<>{});
+		std::transform(std::cbegin(errors), std::cend(errors), std::begin(errors), [](const auto& item) { return item * item; });
+		const auto signal_error = std::accumulate(std::begin(errors), std::end(errors), 0.f);
+
+		std::cout << "Signal error is " << signal_error << std::endl;
 	}
 	return 0;
 }
